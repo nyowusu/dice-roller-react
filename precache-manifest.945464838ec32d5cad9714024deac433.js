c273ddd20b729933d55d73387b8a1e2fad005138
nyowusu <yaw.owusu@hotmail.com>
2019-07-31 03:11:41 +0100

    Updates

---
+++ precache-manifest.945464838ec32d5cad9714024deac433.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c545cc108ff7943686b67e4449cd09e6",
+    "url": "/dice-roller-react/index.html"
+  },
+  {
+    "revision": "38c505781d366f82c41d",
+    "url": "/dice-roller-react/static/css/main.8b443915.chunk.css"
+  },
+  {
+    "revision": "b5dffeaa95314142a179",
+    "url": "/dice-roller-react/static/js/2.cd0b3fd4.chunk.js"
+  },
+  {
+    "revision": "38c505781d366f82c41d",
+    "url": "/dice-roller-react/static/js/main.6e0972b2.chunk.js"
+  },
+  {
+    "revision": "b9eadb38887a2a243e18",
+    "url": "/dice-roller-react/static/js/runtime~main.27446910.js"
+  },
+  {
+    "revision": "30e4c66f8f89c95368cb0c242cedb6f7",
+    "url": "/dice-roller-react/static/media/2.30e4c66f.png"
+  },
+  {
+    "revision": "1c89863075300d48c0fe59166fe2194f",
+    "url": "/dice-roller-react/static/media/3.1c898630.png"
+  },
+  {
+    "revision": "2db95c2f07d5d22139846694a90770b6",
+    "url": "/dice-roller-react/static/media/4.2db95c2f.png"
+  },
+  {
+    "revision": "909f512801a2a6a55d30a464ba2ceace",
+    "url": "/dice-roller-react/static/media/5.909f5128.png"
+  },
+  {
+    "revision": "115f8bbdd7271a9d86c620da313a5b53",
+    "url": "/dice-roller-react/static/media/6.115f8bbd.png"
+  }
+]);
