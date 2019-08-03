a8d695baa2857eee452fca90a5e8a91bd45f666b
nyowusu <yaw.owusu@hotmail.com>
2019-08-04 00:41:08 +0100

    changed dots-container's padding from 0.2em 0.1em 0.1em 0.1em to 0.1em 0.1em 0.1em 0.1em 0.1em

--- src/components/dice-roller/dice-roller.component.jsx
+++ src/components/dice-roller/dice-roller.component.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import DieFaceView from "../die-face-view/die-face-view.component";
 import getRandonInt from "../../randomizer";
 
